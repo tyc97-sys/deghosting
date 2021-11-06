0683adf57954031d7084fd594d764182470096e3
Chen <m10902141@gapps.ntust.edu.tw>
2021-11-06 15:32:04 +0800

    update

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import stackImages
 prefix_path = 'source'
 path1 = os.path.join(prefix_path, '01')
 
-img = load.Load_Image(path1)
+img = load.Load_Image(path1)  # img is a list
+img_gray = load.RGB2GRAY(img) # img_gray is a list
 
-stack_image = stackImages.stackImages(scale=0.5, imgArray=img)
+# stack_image = stackImages.stackImages(scale=0.5, imgArray=img)
 
-cv.imshow("windows_name", stack_image)
+cv.imshow("windows_name", img_gray[1])
 cv.waitKey(0)
