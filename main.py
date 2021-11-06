7e841618478f1b37cee4b75880c7b66214c409c0
Chen <m10902141@gapps.ntust.edu.tw>
2021-11-06 16:39:12 +0800

    update

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ prefix_path = 'source'
 path1 = os.path.join(prefix_path, '01')
 
 img = load.Load_Image(path1)
+gray = load.RGB2GRAY(img)
 
-stack_image = stackImages.stackImages(scale=0.5, imgArray=img)
 
-cv.imshow("windows_name", stack_image)
+# stack_image = stackImages.stackImages(scale=0.5, imgArray=img)
+
+cv.imshow("windows_name", img[0])
 cv.waitKey(0)
