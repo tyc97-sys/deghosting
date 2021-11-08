c811e8cc493e1ae14bcca86574ce535054f55133
Chen <m10902141@gapps.ntust.edu.tw>
2021-11-08 11:41:56 +0800

    test norm

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ img = load.Load_Image(path1)  # img is a list of 2d ndarray
 gray = load.RGB2GRAY(img)  # gray is a list of 2d ndarray
 norm = load.norm(gray)  # norm is a list of 2d ndarray
 
-
+print(norm)
 # stack_image = stackImages.stackImages(scale=0.5, imgArray=img)
 
-cv.imshow("windows_name", img[0])
-cv.waitKey(0)
+# cv.imshow("windows_name", img[0])
+# cv.waitKey(0)
